f61c107253a097a29e32c6581f0e77a45b68ab13
Prajwal Pawar <114561900+PrajwalPawar789@users.noreply.github.com>
2023-09-15 13:20:11 +0530

    First commit: created the directory sturcture and setup express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, function(err) {
+    if(err){
+        console.error(`Couldn't listen on ${port}`);
+    }
+    console.log(`Listening on ${port}`);
+});
+
